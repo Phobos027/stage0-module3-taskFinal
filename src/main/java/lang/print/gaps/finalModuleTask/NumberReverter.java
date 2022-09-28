f39f5e0c942df3a9b5b195bf835fb9d95945edbc
Phobos027 <shkuro.stepan@gmail.com>
2022-09-28 12:17:13 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -4,8 +4,8 @@ public class NumberReverter {
     public void revert(int number) {
         int result;
         result = number % 10 * 100;
-        result += number % 10 * 10;
-        result += number % 10;
+        result += number % 100 / 10 * 10;
+        result += number % 1000 / 100;
         System.out.println(result);
     }
 }
